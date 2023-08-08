54ecd23d6147b7cb533cd4865cbf8b24fa469261
Sunkade <sunkadefasola@gmail.com>
2023-08-08 16:15:51 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-# 3-print_alphabet.py
 
-
-"""Print the alphabet in lowercase, not followed by a new line."""
 for f in range(97, 123):
     if f != 101 and f != 113:
-    print("{}".format(chr(f)), end="")
+        print("{:c}".format(f), end='')
