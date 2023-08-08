722dde8dca63df96b518b940aaee3cf0fba227a7
Sunkade <sunkadefasola@gmail.com>
2023-08-08 15:19:35 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 #2-print_alphabet.py
+
 """Print ther alphabet in lowercase,not followed by new line.""""
 for letter in range(97, 123):
     print("{}".format(chr(letter)), end="")
