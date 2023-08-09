b4e4770ffd1fcb687836168ce9456358ae3b3eaf
Sunkade <sunkadefasola@gmail.com>
2023-08-08 17:56:45 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,10 +2,8 @@
 # 8-uppercase.py
 
 def uppercase(s):
-    result = ""
-    for c in s:
+     for c in s:
         if ord('a') <= ord(c) <= ord('z'):
-            result += chr(ord(c) - ord('a') + ord('A'))
-        else:
-            result += c
-    print(result)
+           c = chr(ord(c) - ord('a') + ord('A'))
+        print("{}".format(c), end="")
+    print("")
