5dd8b28396c15cfd18e55ef6e5fd429ffcfc64ca
Sunkade <sunkadefasola@gmail.com>
2023-08-11 17:14:50 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,41 +1,19 @@
 #!usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div 
+    import sys
 
-def add(a, b):
-    return a + b
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-def sub(a, b):
-    return a - b
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-def mul(a, b):
-    return a * b
-
-def div(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero")
-    return a / b
-
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
-
-ops = {"+": add, "-": sub, "*": mul, "/": div}
-operator = sys.argv[2]
-
-if operator not in ops:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
-
-try:
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-except ValueError:
-    print("Invalid input. <a> and <b> must be integers.")
-    sys.exit(1)
-
-try:
-    result = ops[operator](a, b)
-    print("{} {} {} = {}".format(a, operator, b, result))
-except Exception as e:
-    print("An error occurred:", str(e))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
