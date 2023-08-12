b93db880b8f90359188a84ad90187d653b61bd0c
Sunkade <sunkadefasola@gmail.com>
2023-08-11 18:38:28 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
-import sys
 if __name__ == "__main__":
-    """Print the sum of all arguments."""
-    total = 0
-    for w in range(1, len(sys.argv)):
-        total += int(sys.argv[w])
-        print("{}".format(total))
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
