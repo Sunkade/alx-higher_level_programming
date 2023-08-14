5fb046ed0de3097a68d39fbae98ebc6c7c24ef2c
Sunkade <sunkadefasola@gmail.com>
2023-08-14 10:42:10 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 8-multiple_returns.py
+
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+
+    return (len(sentence), first_char)
