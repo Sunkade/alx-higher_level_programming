759cd0376e6f8ffdd93fbe58d4316c548bee80fd
Sunkade <sunkadefasola@gmail.com>
2023-08-14 10:28:58 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
-        print()
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]))
+            else:
+                print("{:d}".format(row[i]), end=" ")
+        if not row:
+            print()
