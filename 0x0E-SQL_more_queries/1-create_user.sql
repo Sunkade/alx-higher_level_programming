b0f0971493a695cc268a5685a65bae01504b9893
Sunkade <sunkadefasola@gmail.com>
2023-10-18 07:51:44 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,11 @@
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+-- Check if the user already exists
+SELECT user FROM mysql.user WHERE user = 'user_0d_1';
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
