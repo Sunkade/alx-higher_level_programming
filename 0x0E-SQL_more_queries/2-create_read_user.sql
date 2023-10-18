5dd43abe7f618c3689a1437a319032e6c860ff9f
Sunkade <sunkadefasola@gmail.com>
2023-10-18 07:58:49 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,9 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Check if the user already exists
-SELECT user FROM mysql.user WHERE user = 'user_0d_2';
-
--- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege on the hbtn_0d_2 database
