9e2ba55dadc9a748db8f11126a32bee93f0d83de
Sunkade <sunkadefasola@gmail.com>
2023-10-18 08:01:48 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,5 @@
--- Check if the database already exists
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Check if the user already exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'%' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grant SELECT privilege on the hbtn_0d_2 database
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
-
--- Flush privileges to apply changes
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
