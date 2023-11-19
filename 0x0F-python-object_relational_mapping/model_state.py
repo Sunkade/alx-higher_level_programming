321334b9825dc1a7bac85ce27449026cad09ef60
Sunkade <sunkadefasola@gmail.com>
2023-11-19 06:44:04 -0800

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,35 +1,11 @@
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
 
-Base = declarative_base()
+from sqlalchemy import (create_engine)
 
-class State(Base):
-    __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
-
-# Replace 'mysql://user:password@localhost:3306/database' with your actual MySQL connection string
-db_url = 'mysql://user:password@localhost:3306/database'
-engine = create_engine(db_url, echo=True)  # Set echo to True for debugging purposes
-
-# Create the table
-Base.metadata.create_all(engine)
-
-# Example of using the State class with a session
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# Create a new state
-new_state = State(name='New State')
-session.add(new_state)
-session.commit()
-
-# Query the states
-states = session.query(State).all()
-for state in states:
-    print(f"State ID: {state.id}, Name: {state.name}")
-
-# Don't forget to close the session
-session.close()
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
