3f4434407a8e2ae445d945a6eb901910de592ac8
Sunkade <sunkadefasola@gmail.com>
2023-12-01 06:19:30 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
-A script that
-- fetches https://intranet.hbtn.io/status.
-- uses urllib package
+Fetches https://alx-intranet.hbtn.io/status using urllib
 """
 
-if __name__ == '__main__':
-    import urllib.request
+import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        content = res.read()
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
 
     print("Body response:")
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    print("\t- utf8 content: {}".format(utf8_content))
