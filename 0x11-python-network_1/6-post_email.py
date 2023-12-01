cb9c92baf3cf8597ddaaa74909fa84a4bed4e622
Sunkade <sunkadefasola@gmail.com>
2023-12-01 06:56:20 -0800

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
-Sends a POST request to a URL with an email parameter and displays the response body
-"""
-
-import requests
 import sys
+import requests
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {"email": sys.argv[2]}
 
-    data = {'email': email}
-    response = requests.post(url, data=data)
-
-    print(response.text)
+    r = requests.post(url, data=value)
+    print(r.text)
