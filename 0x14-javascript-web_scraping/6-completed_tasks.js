a14b292fe5775bd21b2c0e2388ad74cd9f0b226a
Sunkade <sunkadefasola@gmail.com>
2024-01-04 00:59:57 +0100

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+// web scraper
+
+const request = require('request');
+
+request.get(process.argv[2], { json: true }, (error, response, body) => {
+  if (error) {
+    console.error('Error fetching data:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Unexpected status code: ${response.statusCode}`);
+    return;
+  }
+
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+
+  console.log(tasksCompleted);
+});
