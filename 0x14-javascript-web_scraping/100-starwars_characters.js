ac31e2dc7127936c6dee445add74c81cd969a7d9
Sunkade <sunkadefasola@gmail.com>
2024-01-04 01:05:44 +0100

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,37 +1,25 @@
 #!/usr/bin/node
 
-const request = require('request-promise');
+const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-async function getCharacterNames(characterUrls) {
-  const names = await Promise.all(
-    characterUrls.map(async (characterUrl) => {
-      try {
-        const characterData = await request.get(characterUrl);
-        return JSON.parse(characterData).name;
-      } catch (error) {
-        console.error(`Error fetching character data: ${error}`);
-        return null;
-      }
-    })
-  );
-  return names.filter((name) => name !== null);
-}
-
-async function main() {
-  try {
-    const movieData = await request.get(url);
-    const content = JSON.parse(movieData);
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
     const characters = content.characters;
-    
-    console.log(`Characters in "${content.title}":`);
-    
-    const characterNames = await getCharacterNames(characters);
-    characterNames.forEach((name) => console.log(name));
-  } catch (error) {
-    console.error(`Error fetching movie data: ${error}`);
+    // console.log(characters);
+    for (const character of characters) {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const names = JSON.parse(body);
+          console.log(names.name);
+        }
+      });
+    }
   }
-}
-
-main();
+});
